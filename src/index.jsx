fdc4c073e1748987f4503e8b5709e3675341031f
Ilya Lipakov <ilyalipakov@gmail.com>
2020-03-13 15:28:56 +0300

    Create components

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return(
-        <div>
-            Hello React
-        </div>
-    );
+import Button from "./components/Button.jsx";
+import MessageField from "./components/MessageField.jsx";
+
+class App extends Component{
+
+    state = {
+        messages: [
+            {
+                author: 'Ilya',
+                text: 'Hello'
+            }
+        ],
+    };
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+    }
+
+    render() {
+        return(
+            <div>
+                <input type="text" placeholder="Написать сообщение" />
+                <input type="text" placeholder="Автор" />
+                <Button />
+                <hr/>
+                <MessageField messages={this.state.messages}/>
+            </div>
+        );
+    }
 };
 
 ReactDOM.render(<App />,
